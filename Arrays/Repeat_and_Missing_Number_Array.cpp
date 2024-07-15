a38bedcf9b916bd3b912f1dd719df0344f5071da
Aarya Kasbe <102682317+SolarCat05@users.noreply.github.com>
2024-07-15 20:08:41 +0530

    Create Repeat_and_Missing_Number_Array.cpp

---
+++ Arrays/Repeat_and_Missing_Number_Array.cpp
@@ -0,0 +1,25 @@
+//
+class Solution{
+public:
+    vector<int> findTwoElement(vector<int> arr, int n) {
+        // code here
+        unordered_map<int,int>mp;
+        vector<int>ans;
+        
+        for(int i=0;i<arr.size();i++){
+            if(mp.find(arr[i])!=mp.end()){
+                ans.push_back(arr[i]);
+            }else{
+                mp[arr[i]]=i;
+            }
+        }
+        
+        for(int i=1;i<=n;i++){
+            if(mp.find(i)==mp.end()){
+                ans.push_back(i);
+            }
+        }
+        
+        return ans;
+    }
+};
