41b69f7e2550ed8c62ed031d6bf9d83e0810ac20
Aarya Kasbe <102682317+SolarCat05@users.noreply.github.com>
2024-07-15 19:37:15 +0530

    Update Best_time_to_Buy_and_Sell_Stock.cpp

--- Arrays/Best_time_to_Buy_and_Sell_Stock.cpp
+++ Arrays/Best_time_to_Buy_and_Sell_Stock.cpp
@@ -1,1 +1,31 @@
+//Leetcode Q-121
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int *ptr1=&prices[0];
+        int *ptr2=&prices[1];
+        int max=0;
 
+        for(int i=1;i<prices.size();i++){
+            if(*ptr1>*ptr2){
+                if(i!=prices.size()-1){
+                *ptr1=prices[i];
+                *ptr2=prices[i+1];
+                }
+            }       
+            else{
+                int sum=*ptr2-*ptr1;
+                if(sum>max){
+                    max=sum;
+                }
+
+                if(i!=prices.size()-1){
+                    *ptr2=prices[i+1];
+                }
+            }
+        }
+
+        return max;
+
+    }
+};
