c7c2544d9a5bea76ee342867aa61b6d21ece83b4
Aarya Kasbe <102682317+SolarCat05@users.noreply.github.com>
2024-07-22 21:08:34 +0530

    Create Valid_Palindrome.cpp

---
+++ Strings/Valid_Palindrome.cpp
@@ -0,0 +1,30 @@
+//Leetcode Q-125
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string a;
+        string b;
+
+
+        for(int i=0;i<s.length();i++){
+            if(s[i]==' '){
+                continue;
+            }
+            if(int(s[i])>=65 &&int(s[i])<=90 || int(s[i])>=97 && int(s[i])<=122 || int(s[i])>=48 && int(s[i])<=57){
+                a+=tolower(s[i]);
+            }
+        }
+
+        for(int j=a.length()-1;j>=0;j--){
+            b+=a[j];
+        }
+
+        if(a==b){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+};
