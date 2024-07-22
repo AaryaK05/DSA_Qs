936037bb93224394b80679854c73cea7f1584e46
Aarya Kasbe <102682317+SolarCat05@users.noreply.github.com>
2024-07-22 20:06:28 +0530

    Create Kth-Largest_Element_in_an_Array.cpp

---
+++ Arrays/Kth-Largest_Element_in_an_Array.cpp
@@ -0,0 +1,19 @@
+//Leetcode Q-215
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>>pq;
+
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+        }
+
+        for(int j=0;j<nums.size()-k;j++){
+            cout<<pq.top();
+            pq.pop();
+        }
+
+        return pq.top();
+    }
+};
