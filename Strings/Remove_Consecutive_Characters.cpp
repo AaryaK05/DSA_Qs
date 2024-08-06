f7e0776b09007df4ea353bb23639b3c8501faa7f
Aarya Kasbe <102682317+SolarCat05@users.noreply.github.com>
2024-08-06 21:38:37 +0530

    Create Remove_Consecutive_Characters.cpp

---
+++ Strings/Remove_Consecutive_Characters.cpp
@@ -0,0 +1,22 @@
+//GFG 
+
+class Solution{
+    public:
+    string removeConsecutiveCharacter(string S)
+    {
+        // code here
+        string b;
+        
+        b+=S[0];
+        for(int i=1;i<S.length();i++){
+            if(S[i]==S[i-1]){
+                continue;
+            }else{
+                b+=S[i];
+            }
+        }
+        
+        return b;
+    }
+};
+
