ed0663fb8d16b0c33ba327a5b4cad95cee965609
Aarya Kasbe <102682317+AaryaK05@users.noreply.github.com>
2024-09-18 19:45:00 +0530

    Easy

---
+++ Arrays/Reverse_the_Array.cpp
@@ -0,0 +1,16 @@
+//GFG
+class Solution {
+  public:
+    vector<int> reverseArray(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        for(int i=0;i<n/2;i++){
+            int temp=arr[i];
+            
+            arr[i]=arr[n -i-1];
+            arr[n-i-1]=temp;
+        }
+        
+        return arr;
+    }
+};
