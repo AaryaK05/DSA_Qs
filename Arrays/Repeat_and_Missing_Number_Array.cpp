3814a2921383f0cbcfae0388b38fe15808045d5a
Aarya Kasbe <102682317+AaryaK05@users.noreply.github.com>
2024-09-21 19:54:45 +0530

    Medium

--- Arrays/Repeat_and_Missing_Number_Array.cpp
+++ Arrays/Repeat_and_Missing_Number_Array.cpp
@@ -3,22 +3,32 @@ class Solution{
 public:
     vector<int> findTwoElement(vector<int> arr, int n) {
         // code here
-        unordered_map<int,int>mp;
-        vector<int>ans;
-        
-        for(int i=0;i<arr.size();i++){
-            if(mp.find(arr[i])!=mp.end()){
-                ans.push_back(arr[i]);
-            }else{
-                mp[arr[i]]=i;
-            }
-        }
-        
-        for(int i=1;i<=n;i++){
-            if(mp.find(i)==mp.end()){
-                ans.push_back(i);
-            }
-        }
-        return ans;
+         long long N=arr.size();
+    long long SumofN=N*(N+1)/2;
+    long long SumofArray=0;
+    for(auto it:arr){
+        SumofArray+=it;
+    }
+    // cout<<SumofN<<","<<SumofArray;
+    long long FirstEq=SumofArray-SumofN; //x-y
+    
+    // cout<<"\n";
+    long long SumofSqN=(N*(N+1)*(2*N+1))/6;
+    long long SumofSqArray=0;
+    for(auto it:arr){
+        SumofSqArray+=(long long)it*(long long)it;
+    }
+    // cout<<SumofSqN<<","<<SumofSqArray;
+    
+    long long SecondEq=(SumofSqArray-SumofSqN)/FirstEq;//x+y
+    // cout<<"\n";
+    long long RepeatedEle=(FirstEq+SecondEq)/2;//x
+    long long MissingEle=RepeatedEle-FirstEq;//y
+    // cout<<RepeatedEle<<"|"<<MissingEle<<"\n";
+    
+    
+    
+    
+    return {RepeatedEle,MissingEle};
     }
 };
