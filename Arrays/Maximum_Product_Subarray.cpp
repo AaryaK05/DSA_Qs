b6bafe96f2a9b879f97b748cf72674bb5ef90511
Aarya Kasbe <102682317+AaryaK05@users.noreply.github.com>
2024-09-23 09:24:26 +0530

    Medium

---
+++ Arrays/Maximum_Product_Subarray.cpp
@@ -0,0 +1,23 @@
+//Leetcode 152
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        if(nums.size()==1){
+            return nums[0];
+        }
+
+        int prod1=nums[0];
+        int prod2=nums[0];
+        int result=nums[0];
+
+
+        for(int i=1;i<nums.size();i++){
+            int temp=max({nums[i],prod1 *nums[i],prod2*nums[i]});
+            prod2=min({nums[i],prod1 *nums[i],prod2*nums[i]});
+            prod1=temp;
+            result=max(result,prod1);            
+        }
+
+        return result;
+    }
+};
