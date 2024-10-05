33213dcbe55bb81e76cece0375c4d6e7ea48eb0b
Aarya Kasbe <102682317+AaryaK05@users.noreply.github.com>
2024-10-05 17:26:08 +0530

    Easy-Medium

--- Arrays/Merge_Overlapping_Intervals.cpp
+++ Arrays/Merge_Overlapping_Intervals.cpp
@@ -1,43 +1,25 @@
-//GFG 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <bits/stdc++.h>
-using namespace std;
+//Leetcode-56
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>ans;
 
-vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
+        sort(intervals.begin(),intervals.end());
+        int a=intervals[0][0];
+        int b=intervals[0][1];
 
-    // Sort intervals based on start values
-    sort(arr.begin(),arr.end());
-    unordered_set<int>map;
-    vector<vector<int>> ans;
-    
-    for(int i=0;i<arr.size()-2;i++){
-        if(arr[i][1]<=arr[i+1][1]){
-            int first=min(arr[i][0],arr[i+1][0]);
-            int second=max(arr[i][1],arr[i+1][1]);
-            arr[i+1][0]=first;
-            arr[i+1][1]=second;
-        
-            if(map.find(first)==map.end()&& map.find(second)==map.end()){
-                ans.push_back({first,second});
+        for(auto v:intervals){
+            if(v[0]>b){
+                ans.push_back({a,b});
+                a=v[0];
+                b=v[1];
+            }else if(v[1]>b){
+                b=v[1];
             }
         }
+
+        ans.push_back({a,b});
+        
+        return ans;
     }
-
-    return ans;
-}
-
-// Driver Code
-int main() {
-    vector<vector<int>> arr = {{6, 8}, {1, 9}, {2, 4}, {4, 7}};
-    vector<vector<int>> res = mergeOverlap(arr);
-
-    cout << "The Merged Intervals are: ";
-    for (const vector<int>& interval : res) {
-        cout << "[" << interval[0] << ", " << interval[1] << "] ";
-    }
-    cout << endl;
-
-    return 0;
-}
+};
