f4356d1b324745d8daa15a197b0061d3878ef474
Aarya Kasbe <102682317+AaryaK05@users.noreply.github.com>
2024-10-06 21:20:44 +0530

    Easy

---
+++ Linked List/Linked_List_Cycle.cpp
@@ -0,0 +1,24 @@
+//Leetcode 141
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode *>mp;
+        while(head!=NULL){
+            if(mp.find(head)!=mp.end()){
+                return true;
+            }
+            mp.insert(head);
+            head=head->next;
+        }
+
+        return false;
+    }
+};
