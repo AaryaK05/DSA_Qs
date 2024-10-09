5ecc36774ee6d5262da2fb6615203f9ba67ac673
Aarya Kasbe <102682317+AaryaK05@users.noreply.github.com>
2024-10-09 19:15:21 +0530

    Easy

---
+++ Linked List/Multiply_two_numbers_represented_linked_lists.cpp
@@ -0,0 +1,22 @@
+//Leetcode 
+class solution {
+  public:
+    long long multiplyTwoLists(Node *first, Node *second) {
+        // code here
+        long long MOD = 1000000007;
+      
+        long long n1=0;
+        while(first!=NULL){
+            n1=(n1*10+first->data)%MOD;
+            first=first->next;
+        }
+        
+        long long n2=0;
+        while(second!=NULL){
+            n2=(n2*10+second->data)%MOD;
+            second=second->next;
+        }
+      
+        return (n1*n2)%MOD;
+    }
+};
